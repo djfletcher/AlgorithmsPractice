0731546f1804c9d5bdebf1cc9efa0305da960b3c
Daniel <daniel.j.fletcher2@gmail.com>
2017-08-19 16:40:20 -0700

    Save some problems to do

---
+++ hacker_rank.rb
@@ -0,0 +1,74 @@
+# WARMUP
+
+# Source: https://www.hackerrank.com/challenges/mini-max-sum
+# Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+# Sample Input: 1 2 3 4 5
+# Sample Output: 10 14
+
+
+
+
+# Source: https://www.hackerrank.com/challenges/staircase
+# Observe that its base and height are both equal to , and the image is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+# Sample Input: 6
+# Sample Output:
+#      #
+#     ##
+#    ###
+#   ####
+#  #####
+# ######
+
+
+
+
+
+# Source: https://www.hackerrank.com/challenges/time-conversion`
+# Given a time in -hour AM/PM format, convert it to military (-hour) time.
+# Sample Input: 07:05:45PM
+# Sample Output: 19:05:45
+
+
+
+
+# SEARCH
+
+# Source: https://www.hackerrank.com/challenges/missing-numbers
+# Numeros, the Artist, had two lists  and , such that  was a permutation of . Numeros was very proud of these lists. Unfortunately, while transporting them from one exhibition to another, some numbers were left out of . Can you find the missing numbers?
+# Sample Input: [203 204 205 206 207 208 203 204 205 206], [203 204 204 205 206 207 205 208 203 206 205 206 204]
+# Sample Output: [204 205 206]
+
+
+
+
+# Source: https://www.hackerrank.com/challenges/pairs
+# Given n integers, count the number of pairs of integers whose difference is k.
+# Sample Input: [2, [ 1 5 3 4 2 ] ]
+# Sample Output: 3
+
+
+
+
+# SORTING
+
+# Source: https://www.hackerrank.com/challenges/fraudulent-activity-notifications
+# HackerLand National Bank has a simple policy for warning clients about possible fraudulent account activity. If the amount spent by a client on a particular day is greater than or equal to 2x the client's median spending for the last d days, they send the client a notification about potential fraud. The bank doesn't send the client any notifications until they have at least d prior days of transaction data.
+# Given the value of d and a client's total daily expenditures for a period of n days, find and print the number of times the client will receive a notification over all n days.
+# Sample Input: d = 5, [2 3 4 2 3 6 8 4 5]
+# Sample Output: 2
+
+
+
+
+# DYAMIC PROGRAMMING
+
+# Source: https://www.hackerrank.com/challenges/coin-change
+# Coin change problem => how many different ways are there to make change
+
+
+
+# Source: https://www.hackerrank.com/challenges/candies
+# Alice is a kindergarten teacher. She wants to give some candies to the children in her class.  All the children sit in a line (their positions are fixed), and each  of them has a rating score according to his or her performance in the class.  Alice wants to give at least 1 candy to each child. If two children sit next to each other, then the one with the higher rating must get more candies. Alice wants to save money, so she needs to minimize the total number of candies given to the children.
+# Sample Input: student ratings = [1, 2, 2]
+# Sample Output: 4
+# Here 1, 2, 2 is the rating. Note that when two children have equal rating, they are allowed to have different number of candies. Hence optimal distribution will be 1, 2, 1.
